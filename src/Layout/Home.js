62c565e382b3997d1abc59de9a4ab689cfb88aa9
micah-sanchez <mjsanch4@gmail.com>
2023-02-16 11:39:10 -0700

    Added Add Cards Route

--- src/Layout/Home.js
+++ src/Layout/Home.js
@@ -1,16 +1,35 @@
-import React, { useEffect, useState } from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { useState } from "react";
+import { Route, Switch, useHistory } from "react-router-dom";
 import DeckList from "../Decks/DeckList";
 import NotFound from "./NotFound";
+import CreateDeck from "../Decks/CreateDeck";
+import Study from "../Decks/Study";
+import ViewDeck from "../Decks/ViewDeck"
+import AddCards from "../Cards/AddCards";
 
 function Home() {
+
+    const history = useHistory();
+    const [decks, setDecks] = useState([]);
     
     return (
         <>
-            <button>Create Deck</button>
             <Switch>
-                <Route>
-                    <DeckList exact path="/"/>
+                <Route exact path="/">
+                    <button style={{borderRadius:"10px"}}onClick={() => history.push("/decks/new")}>Create Deck</button>
+                    <DeckList decks={decks} setDecks={setDecks}/>
+                </Route>
+                <Route exact path="/decks/new">
+                    <CreateDeck />
+                </Route>
+                <Route exact path="/decks/:deckId">
+                    <ViewDeck decks={decks}/>
+                </Route>
+                <Route exact path="/decks/:deckId/study">
+                    <Study decks={decks}/>
+                </Route>
+                <Route exact path="/decks/:deckId/cards/new">
+                    <AddCards decks={decks}/>
                 </Route>
                 <Route>
                     <NotFound />
