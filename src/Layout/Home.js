e5a109c1ae5c4312678909f18a795fd6a4d4f7da
micah-sanchez <mjsanch4@gmail.com>
2023-02-16 20:55:59 -0700

    added Edit deck component

--- src/Layout/Home.js
+++ src/Layout/Home.js
@@ -6,30 +6,44 @@ import CreateDeck from "../Decks/CreateDeck";
 import Study from "../Decks/Study";
 import ViewDeck from "../Decks/ViewDeck"
 import AddCards from "../Cards/AddCards";
+import { deleteDeck } from "../utils/api";
+import EditDeck from "../Decks/EditDeck";
 
 function Home() {
 
     const history = useHistory();
     const [decks, setDecks] = useState([]);
+
+    const handleDelete = (id, arrayIndex) => {
+        if (window.confirm("Delete this deck?")) {
+            deleteDeck(id)
+            decks.splice(arrayIndex, 1);
+            setDecks(prev => [...decks])
+            window.open("/")
+        }
+    }
     
     return (
         <>
             <Switch>
                 <Route exact path="/">
                     <button style={{borderRadius:"10px"}}onClick={() => history.push("/decks/new")}>Create Deck</button>
-                    <DeckList decks={decks} setDecks={setDecks}/>
+                    <DeckList decks={decks} setDecks={setDecks} handleDelete={handleDelete}/>
                 </Route>
                 <Route exact path="/decks/new">
                     <CreateDeck />
                 </Route>
                 <Route exact path="/decks/:deckId">
-                    <ViewDeck decks={decks}/>
+                    <ViewDeck decks={decks} handleDelete={handleDelete} />
                 </Route>
                 <Route exact path="/decks/:deckId/study">
                     <Study decks={decks}/>
                 </Route>
                 <Route exact path="/decks/:deckId/cards/new">
-                    <AddCards decks={decks}/>
+                    <AddCards />
+                </Route>
+                <Route exact path="/decks/:deckId/edit">
+                    <EditDeck />
                 </Route>
                 <Route>
                     <NotFound />
