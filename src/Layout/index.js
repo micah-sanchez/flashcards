0b9cf68f98c87ac9145806397fe97146c080c401
micah-sanchez <mjsanch4@gmail.com>
2023-02-17 19:37:07 -0700

    copy for qualified

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Header from "./Header";
-import NotFound from "./NotFound";
 import Home from "./Home";
-import DeckList from "../Decks/DeckList";
 
 // Router
   //Switch  
